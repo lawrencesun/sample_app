3cb7c523d04e89e6cf41b968ae9ee20ee96da9a4
lawrence <yuliang1987@gmail.com>
2013-12-06 15:48:02 +0000

    add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,4 +40,19 @@ describe "Static pages" do
       expect(page).to have_title("Sample App | About")
     end
   end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact' " do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact' " do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Contact')
+    end
+  end
+
+
 end
